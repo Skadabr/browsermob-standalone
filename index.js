710691a7e3586c23f76aff92845d4103ff20ccaf
vladimir-polyakov <vladimir@forma-pro.com>
2015-12-08 20:53:20 +0200

    add 2.1.03

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var path=require("path");
 
 module.exports=function(){
-    return path.resolve(__dirname +"/browsermob-proxy-2.0-beta-10-SNAPSHOT-standalone.jar")
+    return path.resolve(__dirname +"/browsermob-dist-2.1.0-beta-3.jar")
 };
